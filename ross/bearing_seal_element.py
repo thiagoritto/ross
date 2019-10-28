6315d34cc9c0a81cb6ef6bff0f13add016db218c
gabriel <gabrielbachmann@poli.ufrj.br>
2019-10-28 12:21:05 -0300

    fix bearing_seal_element.py doctests

--- ross/bearing_seal_element.py
+++ ross/bearing_seal_element.py
@@ -324,7 +324,7 @@ class BearingElement(Element):
         Examples
         --------
         >>> bearing = bearing_example()
-        >>> bearing.save('BearingElement.toml')
+        >>> bearing.save()
         """
         data = self.load_data(Path(file_name)/'BearingElement.toml')
         if type(self.w) == np.ndarray:
@@ -371,7 +371,7 @@ class BearingElement(Element):
         """
         bearing_elements = []
         bearing_elements_dict = BearingElement.load_data(
-            file_name=''
+            file_name="BearingElement.toml"
         )
         for element in bearing_elements_dict["BearingElement"]:
             bearing = BearingElement(**bearing_elements_dict["BearingElement"][element])
